d4dc1385c8a6d00c6ad42d5b9363b312cbc5f7c7
Sergiy Pazyuk <pazyk.15@gmail.com>
2021-08-10 17:19:00 +0300

    chaged property of copying of config.ini file

--- ex4_Configuration/Providers/FileConfigurationSource.cs
+++ ex4_Configuration/Providers/FileConfigurationSource.cs
@@ -18,18 +18,8 @@ namespace ex4_Configuration.Providers
 
         public IConfigurationProvider Build(IConfigurationBuilder builder)
         {
-            //string filePath = builder.GetFileProvider().GetFileInfo(_filePath).PhysicalPath;
-
-            /**
-             * В цьому місці передаю лише назву файлу в провайдер для того, 
-             * щоб можна було зберегти файл в папці проекту 
-             * а не в папці з exe - файлом (../bin/Debug/net5.0).
-             * 
-             * Коли намагаюсь отримати filePath за допомогою builder,
-             * то він його шукає в папці з exe - файлом, тому для зручності перевірки роблю наступним чином
-             * 
-             */
-            return new FileConfigurationProvider(_filePath);
+            string filePath = builder.GetFileProvider().GetFileInfo(_filePath).PhysicalPath;
+            return new FileConfigurationProvider(filePath);
         }
     }
 }
